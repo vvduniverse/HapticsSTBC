db4711eaf9a0a641ac0b03e8e7bbfb5d3888d77e
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 18:49:24 +0200

    done

--- HapticsSTBC/ContentView.swift
+++ HapticsSTBC/ContentView.swift
@@ -7,10 +7,33 @@
 
 import SwiftUI
 
+class HapticManager {
+    static let instance = HapticManager()
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 20) {
+            Button("success") { HapticManager.instance.notification(type: .success) }
+            Button("warning") { HapticManager.instance.notification(type: .warning) }
+            Button("error") { HapticManager.instance.notification(type: .error) }
+            Divider()
+            Button("soft") { HapticManager.instance.impact(style: .soft) }
+            Button("rigid") { HapticManager.instance.impact(style: .rigid) }
+            Button("medium") { HapticManager.instance.impact(style: .medium) }
+            Button("light") { HapticManager.instance.impact(style: .light) }
+            Button("heavy") { HapticManager.instance.impact(style: .heavy) }
+        }
     }
 }
 
