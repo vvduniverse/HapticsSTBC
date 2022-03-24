096cf0e70e2efb10063026c840d88629e508fe6a
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 18:33:17 +0200

    Initial Commit

---
+++ HapticsSTBC/HapticsSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsSTBCApp.swift
+//  HapticsSTBC
+//
+//  Created by Vahtee Boo on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HapticsSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
